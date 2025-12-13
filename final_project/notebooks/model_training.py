f005716aea090047c74b14ea69e4429a2056a7b4
Drishti Angra <15dan@drishtis-air-2.home>
2025-12-13 18:32:28 +0000

    train test split done

--- final_project/notebooks/model_training.py
+++ final_project/notebooks/model_training.py
@@ -1,4 +1,19 @@
 #%%
-#Loaded cleaned data 
+#import functions
+from venv import create
+from final_project.preprocessing.preprocessing import load_parquet_data, create_sample_split, save_train_test_parquet
+
+
+
+#%% 
+# Load cleaned data 
+
+df_compiled = load_parquet_data()
+df_compiled.head()
 
 #%%
+#Creating a Train Test Split 
+create_sample_split(df_compiled, id_column="ID", training_frac=0.8)
+df_compiled.head()
+
+# %%
--- final_project/preprocessing/preprocessing.py
+++ final_project/preprocessing/preprocessing.py
@@ -1,5 +1,10 @@
 from pathlib import Path
+
+
 import pandas as pd
+import hashlib
+import numpy as np 
+
 
 
 cleaned_data = Path(__file__).resolve().parents[2] / "data" / "cleaned_data.parquet"
@@ -17,3 +22,59 @@ def load_parquet_data() -> pd.DataFrame:
 
 
 
+def create_sample_split(df, id_column, training_frac=0.8):
+    """Create sample split based on ID column.
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        Training data
+    id_column : str
+        Name of ID column
+    training_frac : float, optional
+        Fraction to use for training, by default 0.8
+
+    Returns
+    -------
+    pd.DataFrame
+        Training data with sample column containing train/test split based on IDs.
+    """
+
+    if df[id_column].dtype == np.int64:
+        modulo = df[id_column] % 100
+    else:
+        modulo = df[id_column].apply(
+            lambda x: int(hashlib.md5(str(x).encode()).hexdigest(), 16) % 100
+        )
+
+    df["sample"] = np.where(modulo < training_frac * 100, "train", "test")
+
+    return df
+
+def save_train_test_parquet(df: pd.DataFrame) -> tuple[pd.DataFrame, pd.DataFrame]:
+    """
+    Split a DataFrame into train and test sets using the 'sample' column
+    and save them to the project's data directory as parquet files.
+
+    The files are saved to:
+    house_prices_final_project/data/train.parquet
+    house_prices_final_project/data/test.parquet
+
+    Returns
+    -------
+    tuple[pd.DataFrame, pd.DataFrame]
+        (train_df, test_df)
+    """
+
+    project_root = Path(__file__).resolve().parents[2]
+    data_dir = project_root / "data"
+
+    data_dir.mkdir(exist_ok=True)
+
+    train_df = df[df["sample"] == "train"].drop(columns="sample")
+    test_df = df[df["sample"] == "test"].drop(columns="sample")
+
+    train_df.to_parquet(data_dir / "train.parquet", index=False)
+    test_df.to_parquet(data_dir / "test.parquet", index=False)
+
+    return train_df, test_df
